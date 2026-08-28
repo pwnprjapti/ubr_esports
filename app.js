7230b81c828e1a57367e52cfadd0028d960b5eee
pwnprjapti@gmail.com <pwnprjapti@gmail.com>
2026-08-28 07:50:31 +0530

    increased session age

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(session({
     cookie:{
         httpOnly:true,
         secure:process.env.NODE_ENV === "production",
-        maxAge: 1000*60*60*24 // 1 day
+        maxAge: 1000*60*60*24*365 // 1 year
     }
 }));
 
